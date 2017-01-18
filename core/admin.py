75e365284049caa7f5b02c10d001394b1fe3024e
rg3915 <rg3915@yahoo.com.br>
2017-01-18 21:13:40 -0200

    Admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Books
+from .forms import BookForms
 
-# Register your models here.
+
+@admin.register(Books)
+class BooksAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'title', 'author', 'publication_date',
+                    'pages', 'price')
+    search_fields = ('title', 'author',)
+    list_filter = ('book_type',)
+
+    form = BookForms
